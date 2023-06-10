d0d2dd420744258f1494c29512438e53df39c746
ohurjon <ohurjon@gmail.com>
2023-06-11 01:06:34 +0900

    Update Location Converter

---
+++ src/main/kotlin/kr/sizniss/data/classes/Util.kt
@@ -0,0 +1,56 @@
+package kr.sizniss.data.classes
+
+import com.google.gson.*
+import kr.sizniss.data.DataPlugin
+import org.bukkit.Location
+import org.bukkit.inventory.ItemStack
+
+
+class Util {
+    companion object {
+        @JvmStatic fun toJson(string: String) : JsonObject {
+            return Gson().fromJson(string, JsonObject::class.java)
+        }
+
+        @JvmStatic fun toJson(itemStack: ItemStack) : String {
+            val jsonItem = JsonObject()
+
+            for (data in itemStack.serialize()) {
+                jsonItem.addProperty(data.key,data.value.toString())
+            }
+            return toPrettyJson(jsonItem)
+        }
+
+        @JvmStatic fun toJson(location: Location) : String {
+            val jsonLocation = JsonObject()
+
+            jsonLocation.addProperty("world",location.world!!.name)
+
+            jsonLocation.addProperty("x",location.x)
+            jsonLocation.addProperty("y",location.y)
+            jsonLocation.addProperty("z",location.z)
+
+            jsonLocation.addProperty("yaw",location.yaw)
+            jsonLocation.addProperty("pitch",location.pitch)
+
+            return toPrettyJson(jsonLocation)
+        }
+
+        @JvmStatic fun toLocation(jsonString: String) : Location? {
+            if (jsonString.isEmpty())
+                return null
+            val jsonObject = toJson(jsonString)
+
+            val location = Location(DataPlugin.plugin.server.getWorld(jsonObject.get("world").asString),jsonObject.get("x").asDouble,jsonObject.get("y").asDouble,jsonObject.get("z").asDouble)
+            location.yaw = jsonObject.get("yaw").asFloat
+            location.pitch = jsonObject.get("pitch").asFloat
+
+            return location
+        }
+
+        @JvmStatic fun toPrettyJson(jsonElement: JsonElement): String {
+            return GsonBuilder().setPrettyPrinting().create().toJson(jsonElement)
+        }
+
+    }
+}
